18dd8e15e4dda42159275b22ccf5945f6cc10cb9
Ivo Delev <ivo.delev@wp.pl>
2026-01-16 23:02:08 +0100

    password page
    email validation, password screen,

--- mobile/app/(auth)/landing.tsx
+++ mobile/app/(auth)/landing.tsx
@@ -2,8 +2,27 @@ import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Facebook from "../../assets/images/facebook.png";
 import Google from "../../assets/images/google.png";
+import { useState } from "react";
+import { emailSchema } from "@/validators/emailSchema";
+import { router } from "expo-router";
 
 export default function Landing() {
+  const [email, setEmail] = useState("");
+
+  const handleContinue = () => {
+    const result = emailSchema.safeParse(email);
+
+    if (!result.success) {
+      alert(result.error.issues[0].message);
+      return;
+    }
+
+    router.push({
+      pathname: "/password",
+      params: { email },
+    });
+  };
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="flex-1 bg-white px-6">
@@ -29,18 +48,23 @@ export default function Landing() {
           {/* email + button */}
           <View className="gap-4">
             <TextInput
+              value={email}
+              onChangeText={setEmail}
               placeholderTextColor={"#828282"}
               placeholder="email@domain.com"
               className="border border-border rounded-lg px-4 py-3"
             />
-            <TouchableOpacity className="bg-primary rounded-lg py-4 items-center mb-6">
+            <TouchableOpacity
+              onPress={handleContinue}
+              className="bg-primary rounded-lg py-4 items-center mb-6"
+            >
               <Text className="text-secondaryText">Kontynuuj</Text>
             </TouchableOpacity>
           </View>
           {/* divider */}
           <View className="flex-row items-center mb-6">
             <View className="flex-1 h-px bg-border" />
-            <Text className="mx-4 text-tertiaryText">or</Text>
+            <Text className="mx-4 text-tertiaryText">lub</Text>
             <View className="flex-1 h-px bg-border" />
           </View>
           <View className="gap-6">
--- mobile/app/(auth)/password.tsx
+++ mobile/app/(auth)/password.tsx
@@ -1,10 +1,57 @@
-import { Text, View } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const Password = () => {
+export default function Password() {
   return (
-    <View className="flex-1 p-6">
-      <Text>Password</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-white px-6">
+        {/* logo */}
+        <View className="items-center p-14">
+          <Image
+            className="w-72 h-32"
+            source={{
+              uri: "https://buisnessuserflowgro8d97.blob.core.windows.net/email-assets/FullLogo.png",
+            }}
+          />
+        </View>
+        <View>
+          {/* labelki */}
+          <View className="mb-5">
+            <Text className="text-primaryText text-lg font-medium">
+              Zarejestruj się/Zaloguj się
+            </Text>
+          </View>
+          {/* email + button */}
+          <View className="gap-4">
+            <View className="gap-1">
+              <Text className="text-primaryText font-extralight">
+                Wprowadź hasło, aby przejść dalej.
+              </Text>
+              <TextInput
+                placeholderTextColor={"#828282"}
+                placeholder="Hasło"
+                secureTextEntry={true}
+                className="border border-border rounded-lg px-4 py-3"
+              />
+            </View>
+            <View className="gap-1">
+              <Text className="text-primaryText font-extralight">
+                Powtórz hasło:
+              </Text>
+              <TextInput
+                placeholderTextColor={"#828282"}
+                placeholder="Powtórz hasło"
+                secureTextEntry={true}
+                className="border border-border rounded-lg px-4 py-3"
+              />
+            </View>
+            <TouchableOpacity className="bg-primary rounded-lg py-4 items-center mb-6">
+              <Text className="text-secondaryText">Utwórz konto/Zaloguj</Text>
+            </TouchableOpacity>
+          </View>
+          {/* divider */}
+        </View>
+      </View>
+    </SafeAreaView>
   );
-};
-export default Password;
+}
