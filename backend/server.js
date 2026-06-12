cad3749411ce974d7dac0eb514b65d2b7ee03672
Ivo Delev <ivo.delev@wp.pl>
2026-06-12 07:44:35 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use("/api/company", companyNameRouter);
 
 app.use("/api/contact", contactRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
